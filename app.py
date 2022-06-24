6356aadf87366c3e9a2c7b9bef101dff3eca488d
nrsmac <noahrschill@gmail.com>
2022-06-24 12:11:21 -0600

    Revert "Merge pull request #8 from nrsmac/correct_framerate"
    This reverts commit 1c2554f31ee67f450e4c205b43a822dcb44e4390, reversing
    changes made to 03a4cbdeaaf35cd9477c356970c7e0100c9e9998.

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 from math import sin, cos, pi, pow, sqrt
 from re import X
+import cv2
 from flask import Flask, render_template, Response, request
 from regex import W
-from numpy import full, shape
-
 import time
-import cv2
+
 import socket
 import pickle
-import threading
+from numpy import full, shape
 
 HEADERSIZE = 64
 BLOCK_SIZE = 16348
@@ -54,60 +53,63 @@ def create_app():
     return app
 
 
+#Need to take test csv data, put into numpy array, format "response" with that and a frame, and process it correctly
 def gen_frames():
     #used for saving points as we go
     run_points = []
     returning = False
-    seconds = [] # Seconds is a buffer that is populated in a background thread, receive_thread
     frame, x, y, isRun, angle, magnitude, latitude, longitude = None, None, None, None, None, None, None, None
     start_time = time.time() # start time of the loop
     client = connect_to_server()
-    receiving_thread = threading.Thread(target=receive_stream, args=(client, seconds))
-    receiving_thread.start()
+    #time.sleep(0.05)
 
-    time.sleep(1)  # makes it more likely that seconds is already populated
-    while seconds:
-        for frame_with_data in seconds.pop(0):
-            frame, mat_bytes = frame_with_data 
+    while True:
+        #print("FPS: ", 1.0 / (time.time() - start_time))
+        #start_time = time.time() # start time of the loop
+        request = requestFrameFromServer(client)
+        if request is None:
+            return
+        # frame = zoom(frame, 0.5)
+        frame, mat_bytes = request
 
-            #Initialize run 
-            run = mat_bytes[0]
-            x = int(run[0])
-            y = int(run[1])
-            isRun = run[2]
-            angle = run[3]
-            magnitude = run[4]
-            latitude = run[5]
-            longitude = run[6]
+        # TODO: refactor and clean run code (here until line 96)! 
+        #Initialize run 
+        run = mat_bytes[0]
+        x = int(run[0])
+        y = int(run[1])
+        isRun = run[2]
+        angle = run[3]
+        magnitude = run[4]
+        latitude = run[5]
+        longitude = run[6]
 
-            frame = zoom(frame, SCALE/100)
-            x = int(x * SCALE/100)
-            y = int(y * SCALE/100)
-            magnitude = magnitude * SCALE / 100
+        frame = zoom(frame, SCALE/100)
+        x = int(x * SCALE/100)
+        y = int(y * SCALE/100)
+        magnitude = magnitude * SCALE / 100
 
-            if isRun:
-                if returning:
-                    returning = False
-                    run_points = []
-                run_points.append((x,y))
-            else:
-                returning = True
+        if isRun:
+            if returning:
+                returning = False
+                run_points = []
+            run_points.append((x,y))
+        else:
+            returning = True
 
-            for point in run_points:
-                cv2.circle(frame, point, 2, (0,0,255), -1)
-            cv2.rectangle(frame, (x-RECTANGLE_WIDTH//2,y-RECTANGLE_WIDTH//2), (x+RECTANGLE_WIDTH//2, y+RECTANGLE_WIDTH//2), (0,255,0), 2)
+        for point in run_points:
+            cv2.circle(frame, point, 2, (0,0,255), -1)
+        cv2.rectangle(frame, (x-RECTANGLE_WIDTH//2,y-RECTANGLE_WIDTH//2), (x+RECTANGLE_WIDTH//2, y+RECTANGLE_WIDTH//2), (0,255,0), 2)
 
-            if angle != None and magnitude != None:
-                #TODO - FIX DUMMY VARIABLES
-                x_start, y_start = x, y
-                draw_arrow(frame, angle, x_start, y_start, magnitude)
+        if angle != None and magnitude != None:
+            #TODO - FIX DUMMY VARIABLES
+            x_start, y_start = x, y
+            draw_arrow(frame, angle, x_start, y_start, magnitude)
 
-            # Encode and write image to webpage
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            time.sleep(0.033333333333) # TODO: will this cause issues? Will the lag just get worse each time?  
+        # Encode and write image to webpage
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 def zoom(frame, zoom_factor=2):
@@ -138,29 +140,8 @@ def rescale_frame(frame, percent=75):
     dim = (width, height)
     return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
 
-def receive_stream(client, seconds):
-    while True:
-        second = receive_second(client)
-        if second == None:
-            return None
-        seconds.append(second)
 
-def receive_second(client):
-    '''Receives 30 frames from server. Blocks until 30 frames have been received.'''
-    frames_with_data = []
-    
-    while len(frames_with_data) < 30:
-        response = receive_frame(client)
-        if response is None:
-            return None
-        # frame = zoom(frame, 0.5)
-        img_bytes, mat_bytes = response 
-        frames_with_data.append((img_bytes, mat_bytes))
-    
-    return frames_with_data
-
-def receive_frame(client):
-    '''Receives one frame from server over client socket'''
+def requestFrameFromServer(client):
     full_msg = b''
     new_msg = True
     while True:
